ed3a2e918efd5a1108cbd5cc17e49760a9514b3d
Rahul Gupta-Gunners <rahulbcsit@hotmail.com>
2025-01-18 20:15:26 -0600

    added extension method

--- LinqPractice/ExtensionMethod/Program.cs
+++ LinqPractice/ExtensionMethod/Program.cs
@@ -1,4 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using ExtensionLibrary.Utilties;
 
-//
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+
+        //Demo of groupby
+        var movies = MovieDataProvider.GetMovies(); 
+        MovieLinqUtilities.DisplayGroup(movies, m => m.Rating, MovieLinqUtilities.ConvertGroupedDataToDictionary, "ToDicitonary"); 
+        MovieLinqUtilities.DisplayGroup(movies, m => m.Year, MovieLinqUtilities.ConvertGRoupData, "ToDicitonary");
+
+        MovieLinqUtilities.ConvertToAnother(movies.GroupBy(x => x.Rating).ToArray());
+
+    }
+}
